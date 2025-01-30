b9c3f305e141419e9b3190f4a4f404509a6f65af
Márcio Jorge Gadia <marcio.gadia@gmail.com>
2025-01-29 21:25:20 -0300

    Implementado completamente a AgenciaController

--- src/main/java/br/net/estudos/controller/AgenciaController.java
+++ src/main/java/br/net/estudos/controller/AgenciaController.java
@@ -4,6 +4,8 @@ import org.jboss.resteasy.reactive.RestResponse;
 
 import br.net.estudos.domain.Agencia;
 import br.net.estudos.service.http.AgenciaService;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Context;
@@ -23,5 +25,24 @@ public class AgenciaController {
    public RestResponse<Void> Cadastrar(Agencia agencia, @Context UriInfo uriInfo) {
         this.agenciaService.Cadastrar(agencia);
         return RestResponse.created(uriInfo.getAbsolutePath());
+   }
+
+   @GET
+   @Path("{id}")
+   public RestResponse<Agencia> buscarPorId(Integer id){
+     Agencia agencia = this.agenciaService.BuscarPorId(id);
+     return RestResponse.ok(agencia);
+   }
+
+   @DELETE
+   @Path("{id}")
+   public RestResponse<Void> deletarPorId(Integer id){
+     this.agenciaService.DeletarPorId(id);
+     return RestResponse.ok();
+   }
+
+   public RestResponse<Void> alterar(Agencia agencia) {
+     this.agenciaService.Alterar(agencia);
+     return RestResponse.ok();
    }
 }
